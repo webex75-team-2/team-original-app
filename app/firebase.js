6addd93ee076edaa7115728f8d8d275c8151942f
shun3058 <greeeen.1004.1004@gmail.com>
2024-06-24 20:27:35 +0900

    configを設定しました

---
+++ app/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBt48qvLpqugkvgemqirbBVtiQ7t9HrPPM",
+  authDomain: "team-original-app-d39d9.firebaseapp.com",
+  projectId: "team-original-app-d39d9",
+  storageBucket: "team-original-app-d39d9.appspot.com",
+  messagingSenderId: "233585789453",
+  appId: "1:233585789453:web:b89814d1a10156faa9a344",
+  measurementId: "G-QR6Q54BSTG",
+};
