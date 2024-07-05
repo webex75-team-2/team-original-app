338b3c9e626d7e759da2daf54818f8cf61e4ec9f
shun3058 <greeeen.1004.1004@gmail.com>
2024-07-05 13:29:02 +0900

    いいね機能を作りました

---
+++ app/components/likebutton.jsx
@@ -0,0 +1,68 @@
+import { useCallback, useEffect, useState } from "react";
+import { auth, db } from "../firebase";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  setDoc,
+} from "firebase/firestore";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
+export function LikeButton({ postId }) {
+  const userId = auth.currentUser?.uid;
+
+  const [isLiked, setIsLiked] = useState(null);
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const postRef = doc(db, "posts", postId);
+    const likedUserRef = doc(postRef, "LikedUsers", userId);
+
+    const unsubscribeLikedUser = onSnapshot(likedUserRef, (doc) => {
+      setIsLiked(doc.exists());
+    });
+
+    const likedUsersRef = collection(db, `posts/${postId}/LikedUsers`);
+
+    const unsubscribeLikedCount = onSnapshot(likedUsersRef, (snapshot) => {
+      setLikeCount(snapshot.size);
+    });
+
+    return () => {
+      unsubscribeLikedUser();
+      unsubscribeLikedCount();
+    };
+  }, [userId, postId]);
+
+  const handleClick = useCallback(async () => {
+    if (!userId || isLiked === null) return;
+
+    const likedUserRef = doc(db, "posts", postId, "LikedUsers", userId);
+
+    const userLikePostRef = doc(db, "users", userId, "likePosts", postId);
+    if (isLiked) {
+      await deleteDoc(likedUserRef);
+      await deleteDoc(userLikePostRef);
+    } else {
+      await setDoc(likedUserRef, { userId });
+      await setDoc(userLikePostRef, { postId });
+    }
+  }, [userId, postId, isLiked]);
+
+  if (!userId) return null;
+  if (isLiked === null) return null;
+
+  return (
+    <div>
+      <button onClick={handleClick}>
+        <FontAwesomeIcon icon={faHeart} color={isLiked ? "red" : "grey"} />
+        <p>{likeCount}</p>
+      </button>
+    </div>
+  );
+}
--- app/routes/postIndex.jsx
+++ app/routes/postIndex.jsx
@@ -1,6 +1,7 @@
 import { collection, getDocs } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import { db } from "../firebase";
+import { LikeButton } from "../components/likebutton";
 
 export default function GetPosts() {
   const [posts, setPosts] = useState([]);
@@ -9,6 +10,7 @@ export default function GetPosts() {
     const fetchPosts = async () => {
       const querySnapshot = await getDocs(collection(db, "posts"));
       const postsData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
         ...doc.data(),
       }));
       setPosts(postsData);
@@ -24,6 +26,7 @@ export default function GetPosts() {
           <li key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content}</p>
+            {post.id && <LikeButton postId={post.id} />}
           </li>
         ))}
       </ul>
